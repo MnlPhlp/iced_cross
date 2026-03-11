94a9d38e3824b22819b66042984830a515a28c72
MnlPhlp <online@philipp-manuel.de>
2026-03-11 19:02:33 +0100

    wasm cfg guards

--- crates/iced_cross_build/src/lib.rs
+++ crates/iced_cross_build/src/lib.rs
@@ -2,8 +2,11 @@ mod android;
 
 pub fn build() {
     let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+    let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap_or_default();
     if target_os == "android" {
         android::gen_glue_module();
     }
-    tauri_build::build();
+    if target_arch != "wasm32" {
+        tauri_build::build();
+    }
 }
