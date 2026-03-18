ce49f2cd10a949c9941a4ff3d0e8d13def928a11
MnlPhlp <online@philipp-manuel.de>
2026-03-18 16:31:26 +0100

    added comment

--- examples/counter/gen/android/app/src/main/java/com/example/counter/MainActivity.kt
+++ examples/counter/gen/android/app/src/main/java/com/example/counter/MainActivity.kt
@@ -3,4 +3,7 @@ package com.example.counter
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 
-class MainActivity : TauriNativeActivity() {}
+class MainActivity : TauriNativeActivity() {
+  // This is the main activity of the app, which extends TauriNativeActivity to allow for Java/Kotlin callbacks from native Rust code.
+  // You  can add custom code here and call it from Rust using jni, but it might be easier to use the tauri plugin system  for native code.
+}
